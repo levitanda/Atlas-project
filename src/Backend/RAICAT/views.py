411fab4788bf482dbe9fb71bd9c505a0ea2ede70
Max Yadoshlivyy <yadoshlivyy@gmail.com>
2023-08-06 19:44:54 +0300

    added fragmenatioin functionality

--- src/Backend/RAICAT/views.py
+++ src/Backend/RAICAT/views.py
@@ -4,6 +4,7 @@ from .utils import (
     check_as_for_probes,
     prepare_results_for_frontend,
     dns_between_dates,
+    convert_to_timestamp,
 )
 from .dns_countries_data import countries_data
 
@@ -14,8 +15,53 @@ def dns_data(request, date):
 
 
 def dns_data_line(request, start_date, end_date):
-    # return JsonResponse({"data": countries_data})
-    return JsonResponse({"data": dns_between_dates(start_date, end_date)})
+    start_date_timestamp = convert_to_timestamp(start_date)
+    end_date_timestamp = convert_to_timestamp(end_date)
+    last_day_in_cache = countries_data[-1]["name"]
+    first_date_in_cache = countries_data[0]["name"]
+    (
+        first_date_in_cache_timestamp,
+        last_day_in_cache_timestamp,
+        cached_dates,
+    ) = (
+        convert_to_timestamp(first_date_in_cache),
+        convert_to_timestamp(last_day_in_cache),
+        [item["name"] for item in countries_data],
+    )
+    if (
+        start_date_timestamp >= first_date_in_cache_timestamp
+        and start_date_timestamp <= last_day_in_cache_timestamp
+    ):
+        start_index = cached_dates.index(start_date)
+        if end_date_timestamp <= last_day_in_cache_timestamp:
+            end_index = cached_dates.index(end_date)
+            return JsonResponse(
+                {"data": countries_data[start_index : end_index + 1]}
+            )
+        else:
+            # end_date > last_day_in_cache_timestamp
+            result = countries_data[start_index:]
+            result.extend(dns_between_dates(last_day_in_cache, end_date)[1:])
+            return JsonResponse({"data": result})
+    elif (
+        start_date_timestamp < first_date_in_cache_timestamp
+        and end_date_timestamp >= first_date_in_cache_timestamp
+        and end_date_timestamp <= last_day_in_cache_timestamp
+    ):
+        # start_date < first_date_in_cache_timestamp
+        if end_date_timestamp <= last_day_in_cache_timestamp:
+            end_index = cached_dates.index(end_date)
+            result = dns_between_dates(start_date, first_date_in_cache)
+            result.extend(countries_data[1 : end_index + 1])
+            return JsonResponse({"data": result})
+        else:
+            # end_date > last_day_in_cache_timestamp
+            result = dns_between_dates(start_date, first_date_in_cache)
+            result.extend(countries_data[1:-1])
+            result.extend(dns_between_dates(last_day_in_cache, end_date))
+            return JsonResponse({"data": result})
+    else:
+        return JsonResponse({"data": dns_between_dates(start_date, end_date)})
 
 
 def ipv6_data(request, country, first_date, second_date):
