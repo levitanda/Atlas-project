42cd38860764355aa54c3aad72764001cd388c1b
Max Yadoshlivyy <yadoshlivyy@gmail.com>
2023-08-10 16:10:13 +0300

    added more data in test for readability

--- src/Backend/RAICAT/tests.py
+++ src/Backend/RAICAT/tests.py
@@ -3,9 +3,8 @@ from typing import Dict, List, Union
 from unittest.mock import patch, MagicMock
 import unittest
 
-import pycountry
 from ripe.atlas.sagan import Result
-import pytest
+
 
 from .utils import (
     STOPPED,
@@ -28,7 +27,6 @@ class TestUtils(unittest.TestCase):
         # Test that the function returns 0 for an empty list
         self.assertEqual(compute_average([]), 0)
 
-
     def test_prepare_results_for_frontend(self):
         # Define a dictionary of results
         input_results = {"a": 1.0, "b": 2.0, "c": 3.0}
@@ -40,17 +38,17 @@ class TestUtils(unittest.TestCase):
             "average": 2.0,
         }
         # Test that the prepare_results_for_frontend function returns the expected output
-        self.assertEqual(prepare_results_for_frontend(input_results), expected_output)
+        self.assertEqual(
+            prepare_results_for_frontend(input_results), expected_output
+        )
 
-
-        def test_convert_to_timestamp(self):
-            # Test that the function returns the correct timestamp for a given date
-            self.assertEqual(convert_to_timestamp("2022-01-01"), 1640995200)
-            # Test that the function returns the correct timestamp for a given date and delta_days
-            self.assertEqual(
-                convert_to_timestamp("2022-01-01", delta_days=1), 1641081600
-            )
-
+    def test_convert_to_timestamp(self):
+        # Test that the function returns the correct timestamp for a given date
+        self.assertEqual(convert_to_timestamp("2022-01-01"), 1640995200)
+        # Test that the function returns the correct timestamp for a given date and delta_days
+        self.assertEqual(
+            convert_to_timestamp("2022-01-01", delta_days=1), 1641081600
+        )
 
     # def test_compute_country_code_by_probe_id_dict(self):
     #     expected_output = {
